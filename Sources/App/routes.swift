590096ae19a7d3f8f031c4e4df8b163d0b7a4392
Daniel Spady <danielspady@gmail.com>
2021-02-23 13:28:57 -0500

    Add More Routes
    Add a root Route
    Add a staff route
    Add a contact route
    Add a works route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,10 +1,20 @@
 import Vapor
 
-func routes(_ app: Application) throws {
+public func routes(_ app: Application) throws {
+    // This is the root Route
     app.get { req in
+        return "Welcome to Million Hairs"
+    };
+    app.get("staff") { req in
+        return "Meet our great team"
+    };
+    app.get("contact") { req in
+        return "Get in touch with us"
+    };
+    app.get("title") { req in
         return req.view.render("index", ["title": "Hello Vapor!"])
     };
-    app.get { req in
+    app.get("works") { req in
         return "It works!"
     };
     app.get("hello") { req -> String in
