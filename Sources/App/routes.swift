6ea98f3100acdc52124ad06e1d5854fe1e737789
Daniel Spady <danielspady@gmail.com>
2021-02-25 17:10:22 -0500

    Revert "Route supports dynamic values"
    This reverts commit 0e0113708c3fc3f0a9ff540d3a7fc1b171f1d17c.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -14,39 +14,8 @@ public func routes(_ app: Application) throws {
         let context = [String: String]()
         return req.view.render("index", context)
     };
-    app.get("staff", ":name") { req -> EventLoopFuture<View> in
-        
-        let name = req.parameters.get("name")!
-        
-        // Dummy Data to Display
-        let bios = [
-            "jason": "I'm the Red Power Ranger",
-            "zack": "I'm the Black Power Ranger",
-            "kimberly": "I'm the Pink Power Ranger",
-            "tommy": "I'm the Green Ranger",
-            "billy": "I'm The Blue Power Ranger"
-        ]
-        
-        // Define Struct we'll pass to the template
-        struct StaffView: Codable {
-            var name: String?
-            var bio: String?
-            var isAvailable: Bool?
-        }
-        
-        // Variable with Type of above Struct
-        let context: StaffView
-        
-        // Map staff name to bio and display
-        // Else Create a Blank Instance
-        if let bio = bios[name] {
-            context = StaffView(name: name, bio: bio, isAvailable: true)
-        } else {
-            context = StaffView(name: "", bio: "", isAvailable: false)
-        }
-        
-        // Render template with the context
-        return req.view.render("staff", context)
+    app.get("staff") { req in
+        return "Meet our great team"
     };
     app.get("contact") { req in
         return "Get in touch with us"
